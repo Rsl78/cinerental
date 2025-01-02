c073c7eff2bec9700727a3c4c297e53388d65de0
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2025-01-02 21:25:05 +0600

    template created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MovieList from "./cine/MovieList";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList/>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
